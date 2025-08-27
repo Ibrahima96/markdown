d7d8048a2e4d8211fd4a4c4a50dd722a14eb4ee5
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-08-27 15:33:41 +0000

    feat: initialize React Markdown previewer application
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Created main application component (App.jsx) to handle Markdown input and preview.
    - Implemented textarea for Markdown input and rendered preview using marked-react.
    - Set up main entry point (main.jsx) for the application.
    - Added basic styling in index.css for layout and textarea.
    - Configured Vite for development and build processes.

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import ReactDOM from 'react-dom';
+import Markdown from 'marked-react';
+
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+  //state
+  const refTexarea = useRef();
+
+  useEffect(()=>{
+    refTexarea.current.focus()
+  },[])
+
+  const [showprevMardown,setShowprevMardown]= useState("")
+
+  const showMagicMardown = ()=>{
+    setShowprevMardown(refTexarea.current.value)
+  }
+
+  return (
+    <>
+      <div className="App">
+        <div className="elements">
+          <div className="element">
+            {/* copy */}
+            <textarea ref={refTexarea} defaultValue="# `Prévisualisateur - Believemy`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Believemy](https://believemy.com)" onChange={showMagicMardown}></textarea>
+          </div>
+          <div className="element">
+            {/* previsualisation */}
+            {/* <textarea  value={showprevMardown}></textarea> */}
+            <Markdown>{showprevMardown}</Markdown>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
